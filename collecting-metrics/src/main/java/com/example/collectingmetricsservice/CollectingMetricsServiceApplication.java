1a57962d1991e16d128ae7b2bad2541244787557
Uladzislau <voportno@gmail.com>
2024-03-20 14:59:43 +0100

    comm

--- collecting-metrics/src/main/java/com/example/collectingmetricsservice/CollectingMetricsServiceApplication.java
+++ collecting-metrics/src/main/java/com/example/collectingmetricsservice/CollectingMetricsServiceApplication.java
@@ -11,7 +11,7 @@ public class CollectingMetricsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollectingMetricsServiceApplication.class, args);
-		//
+		////
 	}
 
 }
